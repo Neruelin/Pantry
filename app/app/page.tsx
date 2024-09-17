2e64dbec99a36af98d339c6e47b96c559cf1fb77
neruelin <neruelin@gmail.com>
2024-09-16 19:19:07 -0700

    test reloading by webhook

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
 
     return (
         <main className="home">
-            <h1 className="bg-blue-100">Pretty</h1>
+            <h1 className="bg-blue-100">Pretty like yumi</h1>
             <ControlWrapper />
         </main>
     );
