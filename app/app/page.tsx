6496329213fb4f3ca732faa23ab532fda8b22467
neruelin <neruelin@gmail.com>
2024-09-18 21:46:27 -0700

    testing

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
 
     return (
         <main className="home">
-            <h1 className="bg-blue-100">Pretty like yumi & haylee & meow</h1>
+            <h1 className="bg-blue-100">Pretty like yumi & haylee & meows</h1>
             <ControlWrapper />
         </main>
     );
